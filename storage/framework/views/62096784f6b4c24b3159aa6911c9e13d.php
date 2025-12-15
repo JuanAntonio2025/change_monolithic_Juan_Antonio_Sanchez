0ee526849c78acb9793990799754c4bb6097938a
JuanAntonio2025 <jasanchezdec@gmail.com>
2025-12-15 11:58:23 +0100

    Arreglada sidebar y más

--- storage/framework/views/62096784f6b4c24b3159aa6911c9e13d.php
+++ storage/framework/views/62096784f6b4c24b3159aa6911c9e13d.php
@@ -1,5 +1,5 @@
 <?php $__env->startSection('content'); ?>
-    <div class="container">
+    <div class="container mt-5">
         <h1 class="mb-4">Panel de Administración</h1>
         <div class="row g-4">
 
